cd8588cdf53cd9a1c19a15fc7227afb3f7b9f4b1
milliondigits <milliondigitsofeverything@gmail.com>
2015-12-09 22:52:55 -0800

    Small updates

--- resources/js/dots.js
+++ resources/js/dots.js
@@ -75,19 +75,7 @@
         window.addEventListener('resize', resize);
     }
 
-    function mouseMove(e) {
-        var posx = posy = 0;
-        if (e.pageX || e.pageY) {
-            posx = e.pageX;
-            posy = e.pageY;
-        }
-        else if (e.clientX || e.clientY)    {
-            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-        }
-        target.x = posx;
-        target.y = posy;
-    }
+
 
     function scrollCheck() {
         if(document.body.scrollTop > height) animateHeader = false;
